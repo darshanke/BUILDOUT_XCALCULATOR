4b1679d7c31ab54bdeb02df3edd2a0fe7fc4c3aa
darshan <darshanke646@gmail.com>
2024-10-05 16:05:58 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
-
+import react, { useState } from "react";
+import "./App.css";
 function App() {
+  const [data,setData] = useState('');
+  const [show,setshow] = useState(false);
+  const [details,setdetails] = useState("");
+
+  const buttonValues = [
+    7,
+    8,
+    9,
+    "+",
+    4,
+    5,
+    6,
+    "-",
+    1,
+    2,
+    3,
+    "*",
+    "C",
+    0,
+    "=",
+    "/",
+  ];
+
+
+  const calculateNewValue = (value) => {
+    if (value === "C") {
+      setData("");
+      setshow(false);
+    } else if (value === "=") {
+      try {
+        setshow(true);
+        setdetails(eval(data).toString()); 
+      } catch (error) {
+        setData("Error");
+      }
+    } else if(show){
+      setshow(false);
+      setData(details+value);
+      
+    }else{
+      setData(data+ value)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Calculator</h1>
+      <br />
+
+      <input type="text" style={{ height: "20px" }} value={data} />
+      {show && <p>{details}</p>}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "1rem",
+        }}
+      >
+     
+        <div style={{ width: "260px" }}>
+          {buttonValues.map((item) => (
+            <button
+              onClick={() => calculateNewValue(item)}
+              key={item}
+              style={{
+                width: "50px",
+                height: "50px",
+                margin: "4px",
+                borderRadius: ".5rem",
+                boder: " 10px   solid",
+              }}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
